d949031dce9d8fa1a7fc0bc63c604c9cab670dea
KangZERO650 <77564207+KangZERO650@users.noreply.github.com>
2025-09-29 09:01:00 +0700

    Add files via upload

---
+++ js/mallData.js
@@ -0,0 +1,90 @@
+const cinemaData = {
+            'pakuwon': {
+                name: 'Pakuwon Mall XXI',
+                location: 'Pakuwon Mall, Jl. Puncak Indah Lontar',
+                type: 'Cinema XXI',
+                price: 'Rp 35.000 - Rp 85.000',
+                features: ['8 Studio Premium', 'Dolby Atmos & 4DX', 'Food Court Terintegrasi'],
+                url: 'https://maps.app.goo.gl/BAsXzWsfLh1iXfkZA'
+            },
+            'galaxy': {
+                name: 'Galaxy Mall XXI',
+                location: 'Galaxy Mall Lt. 3, Jl. Dharmahusada Indah',
+                type: 'Cinema XXI',
+                price: 'Rp 30.000 - Rp 60.000',
+                features: ['6 Studio Modern', 'Digital Sound System', 'Parkir Luas'],
+                url: 'https://maps.app.goo.gl/P3XFUamwubh2L47t9'
+            },
+            'trans icon': {
+                name: 'Trans Icon Mall XXI',
+                location: 'Trans Icon Mall, Jl. Manyar Kertoarjo',
+                type: 'Cinema XXI',
+                price: 'Rp 40.000 - Rp 120.000',
+                features: ['7 Studio Eksklusif', 'IMAX Experience', 'Premiere Lounge'],
+                url: 'https://maps.app.goo.gl/S3YgLDWxU76azv7PA'
+            },
+            'maspion': {
+                name: 'CGV Maspion Square',
+                location: 'Maspion Square Lt. 4, Jl. Ahmad Yani',
+                type: 'CGV Cinemas',
+                price: 'Rp 35.000 - Rp 150.000',
+                features: ['5 Studio dengan 4DX', '4DX Motion Seats', 'Special Effects Theater'],
+                url: 'https://maps.app.goo.gl/BoL54qwN7uMBSv7j9'
+            },
+            'marvell': {
+                name: 'CGV Marvell City',
+                location: 'Marvell City Mall Lt. 2, Jl. Ngagel',
+                type: 'CGV Cinemas',
+                price: 'Rp 32.000 - Rp 75.000',
+                features: ['6 Studio Premium', 'Ultra HD Projection', 'Gourmet Snack Bar'],
+                url: 'https://maps.app.goo.gl/ecx6JRTJDrfW4mC6A'
+            },
+            'cito': {
+                name: 'Cinépolis CITO',
+                location: 'City of Tomorrow, Jl. A. Yani',
+                type: 'Cinépolis',
+                price: 'Rp 45.000 - Rp 200.000',
+                features: ['4 Studio VIP', 'Reclining Leather Seats', 'Personal Service'],
+                url: 'https://maps.app.goo.gl/8HLDgGUzHVro7gej7'
+            },
+            'kaza': {
+                name: 'Movimax Kaza City',
+                location: 'Kaza City Mall, Jl. Ir. Soekarno',
+                type: 'Movimax',
+                price: 'Rp 20.000 - Rp 40.000',
+                features: ['3 Studio Family', 'Harga Terjangkau', 'Kids Friendly'],
+                url: 'https://maps.app.goo.gl/SfC6fAZ725fdCAUTA'
+            },
+            'surabaya town square': {
+                name: 'Cinema XXI SUTOS',
+                location: 'Surabaya Town Square, Jl. Hayam Wuruk',
+                type: 'Cinema XXI',
+                price: 'Rp 28.000 - Rp 55.000',
+                features: ['5 Studio Standar', 'Digital Projection', 'Mall Terintegrasi'],
+                url: 'https://maps.app.goo.gl/nkm2qHYhmWfh2wo36'
+            },
+            'tunjungan plaza': {
+                name: 'Cinema XXI Tunjungan Plaza',
+                location: 'Tunjungan Plaza Lt. 5, Jl. Basuki Rahmat',
+                type: 'Cinema XXI',
+                price: 'Rp 35.000 - Rp 75.000',
+                features: ['8 Studio Premium', 'IMAX Theater', 'Premiere Class'],
+                url: 'https://maps.app.goo.gl/3kiTbzZfjxeqo25n7'
+            },
+            'grand city': {
+                name: 'Grand City XXI',
+                location: 'Lantai 4 Grand City, Jl. Walikota Mustajab No.1',
+                type: 'Cinema XXI',
+                price: 'Rp 30.000 - Rp 80.000',
+                features: ['6 Studio Digital', 'Velvet Class', 'Dolby Digital'],
+                url: 'https://maps.app.goo.gl/qqu7wKsN3ddeKExy5'
+            },
+            'ciputra world': {
+                name: 'Cinema XXI Ciputra World',
+                location: 'Ciputra World Lt. 3, Jl. Mayjend Sungkono',
+                type: 'Cinema XXI',
+                price: 'Rp 40.000 - Rp 90.000',
+                features: ['10 Studio Luxury', 'Dolby Atmos', 'VIP Lounge'],
+                url: 'https://maps.app.goo.gl/BsAJujHwzaY7SocB7'
+            }
+        };
